5a50b225ce76f34a7303e6f95fe720e58d0be21a
hacker-gowtham <gowthameng2013@gmail.com>
2023-10-06 20:19:02 +0530

    mail updated

---
+++ index.js
@@ -0,0 +1,15 @@
+//emailjs
+function sendmail(){
+    console.log("gowtham");
+    var params = {
+        from_name: document.getElementById("name").value,
+        email_id: document.getElementById("mail").value,
+        phone_number: document.getElementById("number").value,
+        type: document.getElementById("type").value,
+        message: document.getElementById("message").value,
+     }
+     console.log(params);
+     emailjs.send('service_tbzn7wd','template_qaurv6x',params).then(function(res){
+         alert("success !"+ res.status);
+     });
+}
